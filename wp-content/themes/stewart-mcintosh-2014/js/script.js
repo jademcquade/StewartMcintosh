2e63a501d87f912b19c96098e4a3ded6a43ceb17
Jordan McQuade <Jordan@Jordans-MacBook-Pro.local>
2015-02-22 20:00:10 +0000

    Wip

--- wp-content/themes/stewart-mcintosh-2014/js/script.js
+++ wp-content/themes/stewart-mcintosh-2014/js/script.js
@@ -1,1 +1,10 @@
-console.log('test');
+jQuery( document ).ready(function($) {
+
+	// remove border top for mobile nav dropdown
+    $('.site-navigation li:first').css('border-top','none');
+
+    //style the intro message on the homepage banner
+    $('#intro p:first').css('font-size','24px');
+    $('#intro strong').css({'color':'red', 'font-style':'italic'});
+
+});
