5a0056728211cbc146e6e80d92b11910f34e0728
Jordan McQuade <jordan.mcquade@stv.tv>
2015-03-06 19:42:55 +0000

    Wip

--- wp-content/themes/stewart-mcintosh-2014/footer.php
+++ wp-content/themes/stewart-mcintosh-2014/footer.php
@@ -1,40 +1,33 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains footer content and the closing of the #main and #page div elements.
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
- */
-?>
 
-		<footer id="colophon" class="site-footer" role="contentinfo">
 
-			<?php get_sidebar( 'footer' ); ?>
+	</div><!-- #page -->
 
-				<div id="email">
-					stewart.mcintosh@btinternet.com
+			<footer id="colophon" class="site-footer" role="contentinfo">
+
+			<div id="footerWrap">
+
+				<div id="footerNav">
+
+					<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+						<button class="menu-toggle"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></button>
+						<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+					</nav>
+
 				</div>
 
-					<div id="footerNav">
-
-						<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
-							<button class="menu-toggle"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></button>
-							<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
-							<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-						</nav>
-
+					<div id="emailContact">
+						stewart.mcintosh@btinternet.com
 					</div>
 
 				<div class="site-info">
 					<a target="_blank" href="<?php echo esc_url( __( 'http://www.geo-graphicsdesign.com/' ) ); ?>"><?php printf( __( 'site by %s' ), 'Geo-Graphics' ); ?></a>
 				</div><!-- .site-info -->
 
+			</div>
+
 		</footer><!-- #colophon -->
 
-	</div><!-- #page -->
 
 	<?php wp_footer(); ?>
 	<script src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>
